a06668390cb785065d61fc6c6fafcae27cf076b0
Andrew Kim <akim618@gmail.com>
2018-11-11 17:14:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,2 +1,7 @@
-display_board(board)
-puts board
+
+def display_board(board)
+puts " #{board[0]}| #{board[1]}| #{board[3]}}"
+puts "---------------"
+puts " #{board[4]}| #{board[5]}| #{board[6]}}"
+puts "---------------"
+puts " #{board[7]}| #{board[8]}| #{board[9]}}"
